dbefa3a7566b8a0d3de1ea42feed66d2983ba825
Danielle Meyerson <danimeye@umich.edu>
2017-04-18 13:06:50 -0400

    Added new test cases and a method to the NationalPark class

--- 206_data_access.py
+++ 206_data_access.py
@@ -33,8 +33,8 @@ from bs4 import BeautifulSoup
 # also include a method that will define how the park will be represented when printed to the user. It should make use of each of the instance variables.
 
 class NationalPark(object): # set up location, description, tel_number, site category
+
 	def __init__(self, html_string, current_state):
-		
 		park_info = html_string.find(class_= "col-md-9 col-sm-9 col-xs-12 table-cell list_left")
 
 		self.name = park_info.find('a').text
@@ -49,14 +49,19 @@ class NationalPark(object): # set up location, description, tel_number, site cat
 		return "{0} is a {1} located in {2}. Here is a description of the {0}: {3}".format(self.name, self.site_category, self.state, self.description)
 
 	def get_useful_info(self, html_string):
-		pass 
+		#park_info = html_string.find(class_ = 'col-md-12 col-sm-12  noPadding stateListLinks')
+		park_info = html_string.find(class_ = 'col-md-3 col-sm-3 col-xs-12 result-details-container table-cell list_right')
+		info = park_info.find_all('li')
+		useful_info = [item.find('a').get('href') for item in info]
+		return useful_info
+
 
 # Define a class called Article that accepts an HTML formatted string (representing one article on the National Parks' home page) as input. Use BeautifulSoup to parse through the html data
 # in order to correctly assign values to instance variables. Instance variables for this class should include the article' title, description, and the url to search it.
 
 class Article(object):
-	def __init__(self, html_string):
 
+	def __init__(self, html_string):
 		self.title = html_string.find(class_ = "Feature-title carrot-end").text
 
 		self.description = html_string.find('p').text
@@ -64,23 +69,20 @@ class Article(object):
 		self.article_url = html_string.find('a').get('href')
 
 class State(object):
+
 	def __init__(self, html_string, current_state):
-	
 		state_info = html_string.find_all("li")
-		#print(type(state_info))
 
 		if " Visitors to National Parks" in state_info[1].text:
 			self.visitors = state_info[1].find('strong').text
 		else:
 			self.visitors = 0
-		#print(type(self.visitors))
+		#print(self.visitors)
 
 		if " Economic Benefit from National Park Tourism »" in state_info[2].text:
 			self.econ_benefit = state_info[2].find('strong').text
 		else:
 			self.econ_benefit = 0
-		#self. econ_benefit = int(text_benefit)
-		#self.econ_benefit = Decimal(sub(r'[^\d]', '', text_benefit))
 
 		if " of Rehabilitation Projects Stimulated by Tax Incentives (since 1995) »" in state_info[3].text:
 			self.tax_projects = state_info[3].find('strong').text
@@ -105,7 +107,6 @@ except:
 # Save and return your results in a variable called html_park_list, which should represent a list of html strings with info from each park. 
 
 def get_natlparks_data():
-
 	unique_identifier = "natl_parks_data"
 	state_codes = ['al','ak','as','az','ca','co','ct','de','dc','fl','ga','gu','hi','id','il','in','ia','ks','ky','la','me',
 	'md','ma','mi','mn','ms','mo','mt','ne','nv','nh','nj','nm','ny','nc','nd','mp','oh','ok','or','pa','pr','ri','sc','sd','tn','tx','ut','vt','vi','va','wa','wv','wi','wy']
@@ -129,42 +130,6 @@ def get_natlparks_data():
 	return html_park_list
 		
 
-# Write a function called get_article_links that retrieves the article links on the National Parks Website. These links should specific to each article and will eventually be used to attach to the site's base url to access html data from each article's page. 
-# Save and return these links in a variable called article_links, which represent a list of article link-endings. 
-
-def get_article_links(): 
-
-	base_url = "https://www.nps.gov/index.htm"
-	req_text = requests.get(base_url).text
-	soup = BeautifulSoup(req_text, "html.parser")
-	articles = soup.find(class_ = "Component FeatureGrid")
-	article_links = []
-	for link in articles.find_all('a'):
-		article_links.append(link.get('href'))
-	return article_links 
-
-# Write a function called get_article_html that receives a list of article links as input. For each of these links, fetch html data and add it to your cache file. 
-# Save and return the html data of the articles in a variable called html_article_list, which represent a list of html strings with info from each article.
-
-def get_article_html(article_links):
-	unique_identifier = "article_html"
-	html_article_list = []
-	if unique_identifier in CACHE_DICTION:
-		print("using cached data")
-		html_article_list = CACHE_DICTION[unique_identifier]
-	else:
-		print("accessing article data from internet")
-		for link in article_links:
-			base_url = "https://www.nps.gov" + link
-			html_text = requests.get(base_url)
-			html_article_list.append(html_text.text)
-			print(html_text.url)
-		CACHE_DICTION[unique_identifier] = html_article_list # adding the new data to the cache dictionary
-		cache_file = open(CACHE_FNAME, 'w')
-		cache_file.write(json.dumps(CACHE_DICTION))
-		cache_file.close()
-	return html_article_list 
-
 # Write a function called get_article_data that retrieves html data of the articles on the National Parks homepage. Add this html data to your cache file. 
 # Save and return the html data of the articles in a variable called html_articles, which represents the html data of the National Parks homepage. 
 def get_article_data():
@@ -187,12 +152,6 @@ def get_article_data():
 # Invoke the get_natlparks_data function and save it to a variable called html_park_data.
 html_park_data = get_natlparks_data()
 
-# Invoke the get_article_links function and save it to a variable called article_link_data. 
-article_link_data = get_article_links()
-
-# Using article_link_data, invoke the get_aticle_html function and save it to a variable called html_article_pages. 
-html_article_pages = get_article_html(article_link_data)
-
 # Invoke the get_article_data function and save it to a variable called html_article_data. 
 html_article_data = get_article_data()
 
@@ -206,6 +165,7 @@ for state_html in html_park_data:
 	st_parks = soup.find(class_ = "col-md-9 col-sm-12 col-xs-12 stateCol")
 	for item in st_parks.find_all(class_= "clearfix"):
 		current_obj = NationalPark(item, current_state)
+		#print(current_obj.get_useful_info(item))
 		if current_obj not in repeat_parks:
 			repeat_parks.append(current_obj)
 			park_objs.append(current_obj)
@@ -213,18 +173,8 @@ for state_html in html_park_data:
 # Sort the list of park objects alphabetically by name and save the new list in a variable called sorted_park_objs. 
 sorted_park_objs = [park for park in park_objs]
 sorted_park_objs = sorted(park_objs, key = lambda x: x.name)
-#print(sorted_park_objs[-1])
 
-state_objs = []
-for state_html in html_park_data:
-	soup = BeautifulSoup(state_html, "html.parser")
-	current_state = soup.find(class_ = "ContentHeader").text
-	state_info = soup.find(class_ = "col-md-3 col-sm-12 col-xs-12 stateCol stateCol-right")
-	state_objs.append(State(state_info, current_state))
 
-# for st in state_objs:
-# 	print(st.econ_benefit)
-#print(type(state_info))
 # Using the information stored in html_article_data, create a list of Article objects and save them in a variable called article_objs. 
 article_objs = []
 soup = BeautifulSoup(html_article_data, "html.parser")
@@ -234,6 +184,13 @@ article_list2 = [Article(article) for article in all_articles.find_all(class_ = 
 
 article_objs = article_list1 + article_list2
 
+# Using the information stored in html_park_data, create a list of State objects and save them in a variable called state_objs. 
+state_objs = []
+for state_html in html_park_data:
+	soup = BeautifulSoup(state_html, "html.parser")
+	current_state = soup.find(class_ = "ContentHeader").text
+	state_info = soup.find(class_ = "col-md-3 col-sm-12 col-xs-12 stateCol stateCol-right")
+	state_objs.append(State(state_info, current_state))
 
 # Create a database file called national_parks.db. In the database, you will make three tables called Parks, Articles, and States as follows:
 
@@ -257,7 +214,7 @@ article_objs = article_list1 + article_list2
 # - econ_benefit (containing the amount of money representing the economic benefit from national park tourism)
 # - tax_projects (containing the amount of money representing the tax incentives used to stimulate projects)
 
-# You should load all of the above information into their respective tables
+# You should load all of the above information into their respective tables.
 
 conn = sqlite3.connect("national_parks.db")
 cur = conn.cursor()
@@ -300,13 +257,7 @@ for st in state_objs:
 
 conn.commit()
 
-
-
-
-		
-
-
-
+# QUERIES GO HERE:
 
 
 
@@ -319,22 +270,32 @@ conn.commit()
 ###################### TEST CASES #########################
 
 class TestPlan(unittest.TestCase):
+
 	def test_get_data(self):
-		html_park_data = get_natlpark_data()
+		html_park_data = get_natlparks_data()
 		self.assertEqual(type(html_park_data), type([]), 'Testing that the return value of the get_natlpark_data() function is a list')
 	def test_park_name(self):
 		self.assertEqual(type(sorted_park_objs[0].name), type(""), 'Testing that the name of the park is a string')
-	def test_articleText(self):
+	def test_article_text(self):
 		self.assertEqual(type(article_objs[0].title), type(""), 'Testing that the title of the article is a string')
-	def test_description_list():
+	def test_description_list(self):
 		self.assertEqual(type(sorted_park_objs), type([]), 'Testing that the sorted_park_objs is a list')
 	def test_park_list(self):
-		self.assertEqual(sorted_park_objs[0].name, "Zion", 'Testing that the last park in sorted_park_objs is Zion')
-	def test_article_list(self):
-		article1 = Article()
-		self.assertEqual(type(article_list[0]), type(article1), 'Testing that the first element in the list of article instances, article_list, is an Article object')
-	# def test_mentioned_parks(self):
-	# 	self.assertEqual(type(mentioned_parks) type({'state':'park'})
+		self.assertEqual(sorted_park_objs[-1].name, "Zion", 'Testing that the last park in sorted_park_objs is Zion')
+	def test_state_name(self):
+		html_file = open('Michigan (U.S. National Park Service).htm', 'r')
+		file_contents = html_file.read()
+		html_file.close()
+		soup = BeautifulSoup(file_contents, "html.parser")
+		self.assertEqual(soup.find(class_ = "ContentHeader").text.strip(), 'Michigan', 'Testing the assignment statement leads to the correct state name')
+	def test_state_visitors(self):
+		html_file = open('Michigan (U.S. National Park Service).htm', 'r')
+		file_contents = html_file.read()
+		html_file.close()
+		soup = BeautifulSoup(file_contents, "html.parser")
+		current_state = soup.find(class_ = "ContentHeader").text
+		state_info = soup.find(class_ = "col-md-3 col-sm-12 col-xs-12 stateCol stateCol-right")
+		self.assertEqual(State(state_info, current_state).visitors, '2,386,613', 'Testing that the number of visitors is correct')
 	def test_DB(self):
 		conn = sqlite3.connect('national_parks.db')
 		cur = conn.cursor()
@@ -343,7 +304,6 @@ class TestPlan(unittest.TestCase):
 		self.assertEqual(len(result), 644, 'Testing that the number of national parks in the database is 644')
 		conn.close()
 	
-
 
 
 
